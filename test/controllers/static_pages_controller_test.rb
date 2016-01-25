cfd31a968e9b3809ceb71dcab3365909e53f6274
rushabh <rusamgr8@gmail.com>
2016-01-25 11:40:17 +0000

    static pages tested

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title","Home | Review System"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title","Help | Review System"
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title","About | Review System"
+  end
 end
